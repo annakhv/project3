f7c7b0b9596fa9767b9da95970c58b8ebe6b728f
annakhv <annakhvedelidze@gmail.com>
2021-01-05 15:42:28 +0400

    message

--- orders/urls.py
+++ orders/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
-    path("logout",  views.logout_view, name="logout")
+    path("logout",  views.logout_view, name="logout"),
+    path("register",  views.register_view, name="register"),
+    path("registerPage",  views.registerPage_view, name="registerPage")
+
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,20 +1,37 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.urls import reverse
 
 def index(request):
     if not request.user.is_authenticated:
-        return render(request, "orders/login.htmls", {"message": None})
+        return render(request, "orders/login.html", {"message": None})
     context ={
         "user" : request.user
     }
     return render(request, "orders/user.html", context)
 
+def registerPage_view(request):
+    return render(request, "orders/register.html", {"message": None})
+
+def register_view(request):
+    print("hello")
+    username=request.POST["username"]    
+    password=request.POST["password"]
+    email=request.POST["password"]
+    fname=request.POST["fname"]
+    lname=request.POST["lname"]
+    user = User.objects.create_user(username, email, password)
+    user.first_name=fname
+    user.last_name=lname
+    user.save()
+    return render(request, "orders/login.html", {"message": "user is successfuly registered, login in now with your credentials"})
 
 
 def login_view(request):
-    username=request.POST["username"]    
+    username=request.POST["username"] 
+
     password=request.POST["password"]
     user=authenticate(request, username=username, password=password)
     if user is not None:
