e52c74c91937ed57a6eba11b8e16809372edb472
annakhv <annakhvedelidze@gmail.com>
2021-01-06 18:52:43 +0400

    somejs

--- orders/static/orders/scripts.js
+++ orders/static/orders/scripts.js
@@ -3,7 +3,8 @@
 
 function addpizza() {
 
-   var all=document.querySelectorAll('input[name="regular"]');
+   let id=event.srcElement.id
+   var all=document.querySelectorAll(`input[name=${id}]`);
    all.forEach(element => {
         if( element.checked === true){
               let pizza=element.value;
@@ -11,16 +12,22 @@ function addpizza() {
               li.innerHTML=pizza;
               document.querySelector("#itemList").append(li);
               element.checked =false;
-              console.log(pizza)
+              if (pizza.includes("topping")){
+                   let h3=document.createElement("h3");
+                    h3.innerHTML=" add toppings before moving on!";
+                   document.querySelector("#itemList").append(h3);
+                  
+              }
               document.querySelector("#orderbutton").classList.remove("visibility");
               document.querySelector("#add").classList.remove("visibility");
-        }
           
-       
+        }   
       
    });
 
-   
+}
 
+function addtopping() {
 
+      
 }
